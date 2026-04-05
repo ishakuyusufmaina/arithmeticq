537bd33b2095ae9946d81e6f73ed8c9030d93a52
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-04-05 03:17:28 +0100

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,8 +1,9 @@
 const DIFFICULTY = [
-"Easy (1 - 10)",
-"Hard (1-20)",
-"Harder (1-30)",
-"Hardest (1 - 40)"
+{level: 10, label: "Easer (1 - 10)"},
+{level: 20, label: "Easy (10 - 20)"},
+{level: 30, label: "Medium (20 - 30)"},
+{level: 40, label: "Hard (40 - 50)"},
+{level: 50, label: "Difficult (50 - 60)"}
 ];
 
 const FINISH = "FINISH";
